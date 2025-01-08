a0e64f03b1657778e76e6823d8eb16f370507f60
burahan206 <141344974+burahan206@users.noreply.github.com>
2025-01-08 20:15:12 +0700

    feat: single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,9 +55,9 @@ const addProduct = async (req, res) => {
 const listProduct = async (req, res) => {
     try {
 
-        const products =await productModel.find({});
-        res.json({success:true,products})
-        
+        const products = await productModel.find({});
+        res.json({ success: true, products })
+
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
@@ -67,15 +67,29 @@ const listProduct = async (req, res) => {
 }
 
 // function for removing product
-const removeProduct = async (res, req) => {
-
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Removed" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 
 }
 
 // function for single product
-const singleProduct = async (res, req) => {
+const singleProduct = async (req, res) => {
+    try {
 
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 
 }
