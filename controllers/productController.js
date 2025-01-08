0c1baf738cd1cf68ab94cc8b7742dbaf1c19fda9
burahan206 <141344974+burahan206@users.noreply.github.com>
2025-01-08 19:57:58 +0700

    feat: list product

--- controllers/productController.js
+++ controllers/productController.js
@@ -52,8 +52,16 @@ const addProduct = async (req, res) => {
 }
 
 // function for list product
-const listProduct = async (res, req) => {
+const listProduct = async (req, res) => {
+    try {
 
+        const products =await productModel.find({});
+        res.json({success:true,products})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 
 }
